0463f2525b5a421fa3a46024d2d28d842ea05b90
AndrewMuthumbi <joeandrews176@gmail.com>
2024-04-27 15:15:00 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h ; implementation of a calculator program

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+int main(int argc, char *argv[])
+{
+int num1;
+char *operator;
+int num2;
+int (*operation)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+return (98);
+}
+
+num1 = atoi(argv[1]);
+operator = argv[2];
+num2 = atoi(argv[3]);
+
+operation = get_op_func(operator);
+
+if (operation == NULL)
+{
+printf("Error\n");
+return (99);
+}
+
+if ((*operator == '/' || *operator == '%') && num2 == 0)
+{
+printf("Error\n");
+return (100);
+}
+
+printf("%d\n", operation(num1, num2));
+
+return (0);
+}
