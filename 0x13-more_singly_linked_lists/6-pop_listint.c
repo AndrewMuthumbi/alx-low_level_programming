65ac40d9f138a45da0393f59402bf4dfa0fea8e2
AndrewMuthumbi <joeandrews176@gmail.com>
2024-05-02 15:21:01 -0700

    fixed betty coding style on 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -5,7 +5,8 @@
  * pop_listint - deletes the head node of a listint_t linked list
  * @head: pointer to a pointer to the head of the list
  *
- * Return: the data (n) of the head node that was deleted, or 0 if the list is empty
+ * Return: the data (n) of the head node that was deleted,
+ *         or 0 if the list is empty
  */
 int pop_listint(listint_t **head)
 {
@@ -13,7 +14,7 @@ int data;
 listint_t *temp;
 
 if (*head == NULL)
-return 0;
+return (0);
 
 temp = *head;
 *head = (*head)->next;
