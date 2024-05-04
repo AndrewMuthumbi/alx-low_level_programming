368efc62f2b0bff54b6c1523e2ee30a87fff26d5
AndrewMuthumbi <joeandrews176@gmail.com>
2024-05-04 15:27:24 -0700

    TASK 3; a program that copies the content of a file to another file

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * print_error_and_exit - Prints an error message and exits with the given code
+ * @msg: The error message to print
+ * @code: The exit code
+ */
+void print_error_and_exit(char *msg, int code)
+{
+dprintf(STDERR_FILENO, "%s\n", msg);
+exit(code);
+}
+
+/**
+ * copy_file - Copies the content of a file to another file
+ * @src_fd: The file descriptor of the source file
+ * @dest_fd: The file descriptor of the destination file
+ */
+void copy_file(int src_fd, int dest_fd)
+{
+ssize_t bytes_read, bytes_written;
+char buffer[BUF_SIZE];
+
+while ((bytes_read = read(src_fd, buffer, BUF_SIZE)) > 0)
+{
+bytes_written = write(dest_fd, buffer, bytes_read);
+if (bytes_written != bytes_read)
+print_error_and_exit("Error: Can't write to file", 99);
+}
+
+if (bytes_read == -1)
+print_error_and_exit("Error: Can't read from file", 98);
+}
+
+/**
+ * main - Copies the content of a file to another file
+ * @argc: The number of arguments
+ * @argv: The array of arguments
+ *
+ * Return: 0 on success, or exit with error codes 97, 98, 99, or 100
+ */
+int main(int argc, char *argv[])
+{
+int src_fd, dest_fd;
+
+if (argc != 3)
+print_error_and_exit("Usage: cp file_from file_to", 97);
+
+src_fd = open(argv[1], O_RDONLY);
+if (src_fd == -1)
+print_error_and_exit("Error: Can't read from file", 98);
+
+dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+if (dest_fd == -1)
+print_error_and_exit("Error: Can't write to file", 99);
+
+copy_file(src_fd, dest_fd);
+
+if (close(src_fd) == -1 || close(dest_fd) == -1)
+print_error_and_exit("Error: Can't close fd", 100);
+
+return (0);
+}
