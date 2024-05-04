854ad5f47888af05f1232a17847e822bdb557528
AndrewMuthumbi <joeandrews176@gmail.com>
2024-05-04 16:05:20 -0700

    Corrected task 3 & created a RADME.md file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,13 +6,15 @@
 #define BUF_SIZE 1024
 
 /**
- * print_error_and_exit - Prints an error message and exits with the given code
+* print_error_and_exit - Prints an error message
+*           including the file name and exits with the given code
  * @msg: The error message to print
+ * @filename: The name of the file causing the error
  * @code: The exit code
  */
-void print_error_and_exit(char *msg, int code)
+void print_error_and_exit(char *msg, char *filename, int code)
 {
-dprintf(STDERR_FILENO, "%s\n", msg);
+dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
 exit(code);
 }
 
@@ -20,8 +22,10 @@ exit(code);
  * copy_file - Copies the content of a file to another file
  * @src_fd: The file descriptor of the source file
  * @dest_fd: The file descriptor of the destination file
+ * @src_name: The name of the source file
+ * @dest_name: The name of the destination file
  */
-void copy_file(int src_fd, int dest_fd)
+void copy_file(int src_fd, int dest_fd, char *src_name, char *dest_name)
 {
 ssize_t bytes_read, bytes_written;
 char buffer[BUF_SIZE];
@@ -30,11 +34,11 @@ while ((bytes_read = read(src_fd, buffer, BUF_SIZE)) > 0)
 {
 bytes_written = write(dest_fd, buffer, bytes_read);
 if (bytes_written != bytes_read)
-print_error_and_exit("Error: Can't write to file", 99);
+print_error_and_exit("Error: Can't write to file", dest_name, 99);
 }
 
 if (bytes_read == -1)
-print_error_and_exit("Error: Can't read from file", 98);
+print_error_and_exit("Error: Can't read from file", src_name, 98);
 }
 
 /**
@@ -49,20 +53,20 @@ int main(int argc, char *argv[])
 int src_fd, dest_fd;
 
 if (argc != 3)
-print_error_and_exit("Usage: cp file_from file_to", 97);
+print_error_and_exit("Usage: cp file_from file_to", "", 97);
 
 src_fd = open(argv[1], O_RDONLY);
 if (src_fd == -1)
-print_error_and_exit("Error: Can't read from file", 98);
+print_error_and_exit("Error: Can't read from file", argv[1], 98);
 
 dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 if (dest_fd == -1)
-print_error_and_exit("Error: Can't write to file", 99);
+print_error_and_exit("Error: Can't write to file", argv[2], 99);
 
-copy_file(src_fd, dest_fd);
+copy_file(src_fd, dest_fd, argv[1], argv[2]);
 
 if (close(src_fd) == -1 || close(dest_fd) == -1)
-print_error_and_exit("Error: Can't close fd", 100);
+print_error_and_exit("Error: Can't close fd", "", 100);
 
 return (0);
 }
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,30 @@
+# 0x15. C - File I/O
+
+This repository contains C programs that demonstrate various file I/O operations, including creating, reading, writing, and copying files. Each program is designed to fulfill specific tasks related to file manipulation.
+
+## Contents
+
+1. [Description](#description)
+2. [Requirements](#requirements)
+3. [Usage](#usage)
+4. [Files](#files)
+5. [Compilation](#compilation)
+6. [License](#license)
+
+## Description
+
+The programs in this repository demonstrate the following file I/O operations:
+
+- `0-read_textfile.c`: Reads a text file and prints its content to the POSIX standard output.
+- `1-create_file.c`: Creates a file with specified content.
+- `2-append_text_to_file.c`: Appends text to the end of a file.
+- `3-cp.c`: Copies the content of one file to another.
+
+Each program comes with a `main.c` file that demonstrates the usage of the respective function.
+
+## Requirements
+
+- The programs are written in C and compiled using GCC.
+- All programs are developed and tested on Ubuntu 20.04 LTS.
+- The code must adhere to the Betty style guide.
+- The programs use standard C libraries such as `stdio.h`, `stdlib.h`, `unistd.h`, and `fcntl.h`.
