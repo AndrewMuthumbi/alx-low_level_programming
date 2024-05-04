20d99503cc0ae4ee22f8fd2230c41a46ba716215
AndrewMuthumbi <joeandrews176@gmail.com>
2024-05-04 16:49:40 -0700

    TASK 3, edited to give the correct output and edited betty errors.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,14 @@
 section .data
-    hello db "Hello, Holberton", 10, 0  ; Null-terminated string with newline character
+    format db 'Hello, Holberton', 10, 0  ; Null-terminated string with newline character
 
 section .text
-    extern printf  ; Import printf function from C standard library
-
-global _start
+    global _start
+    extern printf
 
 _start:
-    ; Print the string using printf
-    mov rdi, hello  ; Load the address of the string
-    call printf     ; Call printf function
+    mov rdi, format
+    call printf
 
-    ; Exit the program
-    mov rax, 60          ; Exit syscall number
-    xor rdi, rdi         ; Return 0 status
+    mov rax, 60
+    xor rdi, rdi
     syscall
