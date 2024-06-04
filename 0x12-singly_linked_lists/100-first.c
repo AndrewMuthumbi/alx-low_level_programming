f05087a9967825b285e9c0cba04a59243d68fbba
AndrewMuthumbi <joeandrews176@gmail.com>
2024-06-04 12:29:23 -0700

    edited 100-first.c get the correct output upon compilation

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 /**
- * __attribute__((constructor)) first - Prints a message before main function
- * is executed
+ * __attribute__((constructor)) first - Prints a message before
+ * main function is executed
  */
 void __attribute__((constructor)) first(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
